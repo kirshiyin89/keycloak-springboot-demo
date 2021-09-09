89bbb466c6977623d18955f89099aead6ce22ea1
kirshiyin89 <kristina.nikolova1989@gmail.com>
2021-09-09 22:34:00 +0200

    added test file

---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DummyTest {
+
+    @Test
+    public void test() {
+        String str1="This is a dummy test";
+        assertEquals("This is a dummy test", str1);
+    }
+}
